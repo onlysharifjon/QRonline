96b25098df0501b8520145f96b7c95c64db965a6
onlysharifjon <mominovsharif12@gmail.com>
2025-04-23 04:03:52 +0500

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.api.endpoints import image, view,register
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # yoki ["http://localhost:5173"] — xavfsizroq
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app = FastAPI()
 
 app.include_router(image.router)
