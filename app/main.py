be6083db9abab9e2fd1ea13a7e5aa76443978eeb
onlysharifjon <mominovsharif12@gmail.com>
2025-04-23 04:06:31 +0500

    add CORS

--- app/main.py
+++ app/main.py
@@ -4,13 +4,7 @@ from app.api.endpoints import image, view,register
 
 from fastapi.middleware.cors import CORSMiddleware
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # yoki ["http://localhost:5173"] — xavfsizroq
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 app = FastAPI()
 
@@ -20,5 +14,12 @@ app.include_router(view.router)
 # media papkani statik fayllar uchun ochamiz
 app.mount("/media", StaticFiles(directory="media"), name="media")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # yoki ["http://localhost:5173"] — xavfsizroq
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(register.router)
